0be5bf5180f105d49ea0412cd78319490a93f263
Dave Hornigan <dave@hornigan.com>
2023-07-23 01:08:41 +0300

    fixed file permissions, added docker compose config

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -5,6 +5,7 @@ namespace App\EventListener;
 use App\Exception\ConstraintViolationHttpException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class ExceptionListener
 {
@@ -16,6 +17,9 @@ class ExceptionListener
             'message' => $exception->getMessage(),
             'code' => $exception->getCode() === 0 ? 500 : $exception->getCode()
         ];
+        if ($exception instanceof HttpException) {
+            $responseData['code'] = $exception->getStatusCode();
+        }
         if ($exception instanceof ConstraintViolationHttpException) {
             $responseData['errors'] = $exception->getErrors();
         }
